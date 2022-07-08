c29fcede1ec1ca5bdd64f073024245ba6d352b4d
hammerflyhub <ruohan.li@outlook.de>
2022-07-09 01:17:45 +0200

    first push

---
+++ app.py
@@ -0,0 +1,48 @@
+print('Ruohan Li')
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+price = 10
+rating = 4.9
+name = 'Mosh'
+is_published = True
+print(price)
+
+namae = 'John Smith'
+age = 20
+is_new = True
+name = input("What is your name?") 
+favorite_color = input('Which color do you like?')
+print(name+ ' likes ' + favorite_color)
+
+birth_year = input("Birth year")
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+email = '''
+Hi John,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+
+'''
+print(email)
+
+course = 'Python for Beginners'
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+
+
+
---
+++ module.py
@@ -0,0 +1,5 @@
+import utils
+
+numbers = [10,3,6,2]
+
+print(utils.find_max(numbers))
